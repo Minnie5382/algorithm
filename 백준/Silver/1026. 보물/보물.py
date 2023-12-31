e40d6cb2ccb67421cfe722a933181bac8a27858c
minn <97179789+Minnie5382@users.noreply.github.com>
2023-12-31 16:53:12 +0900

    [Silver IV] Title: 보물, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+B.sort()
+A.sort(reverse=True)
+sum = 0
+for i in range(N):
+  sum += A[i]*B[i]
+print(sum)
