7e86dae43c3a23de033f2759a0ac02de66885b7c
minn <97179789+Minnie5382@users.noreply.github.com>
2024-01-07 21:16:13 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 180 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
@@ -0,0 +1,9 @@
+N, M = map(int, input().split())
+list = list(map(int, input().split()))
+
+for i in range(M):
+  list.sort()
+  temp = list[0]+list[1]
+  list[0] = temp
+  list[1] = temp
+print(sum(list))
