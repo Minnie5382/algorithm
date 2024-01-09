f345b93d2696417d814edf3ff75c0b406957c7d3
minn <97179789+Minnie5382@users.noreply.github.com>
2024-01-09 11:25:40 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.py
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.py
@@ -1,16 +1,9 @@
-N, M, K = map(int, input().split())
+N, M, K = map(int,input().split())
 
-count = 0
-for i in range(50):
-    if N-2 >= 0 and M-1 >= 0:
-        N -= 2
-        M -= 1
-        count += 1
+for i in range(K):
+    if N//2 >= M:
+        N -= 1
     else:
-        break
-
-if (N+M) >= K:
-    print(count)
-else:
-    count -= (K-(N+M)+2)//3
-    print(count)
+        M -= 1
+        
+print(min(N//2, M))
