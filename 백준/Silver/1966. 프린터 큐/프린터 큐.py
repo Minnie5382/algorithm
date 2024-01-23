ad83c88cac4e80175a9c82a6562930e78ea26264
minn <97179789+Minnie5382@users.noreply.github.com>
2024-01-24 00:50:53 +0900

    [Silver III] Title: 프린터 큐, Time: 76 ms, Memory: 34028 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,23 +1,21 @@
 from collections import deque
-test = int(input())
 
+test = int(input())
 for _ in range(test):
-    count = 0
-    popped = []
     popped_index = []
     N, M = map(int, input().split())
+
     priority = list(map(int, input().split()))
-    # print()
-    index = [i for i in range(N)]
-    queue = deque(priority)
-    index_queue = deque(index)
-    while queue:
-        if queue[0] == max(queue):
-            popped.append(queue.popleft())
+    priority_queue = deque(priority)
+
+    index_queue = deque([i for i in range(N)])
+
+    while priority_queue:
+        if priority_queue[0] == max(priority_queue):
+            priority_queue.popleft()
             popped_index.append(index_queue.popleft())
-            count += 1
         else:
-            queue.rotate(-1)
+            priority_queue.rotate(-1)
             index_queue.rotate(-1)
 
     print(popped_index.index(M)+1)
